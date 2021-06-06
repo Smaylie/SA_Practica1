570670dff2ed9e88262be89c293ab1b9cebb39ee
Smaylie <splayer2012@gmail.com>
2021-06-06 17:17:12 -0600

    Test Erroneo

--- App/src/app/app.component.spec.ts
+++ App/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('App');
+    expect(app.title).toEqual('Practica');
   });
 
   it('should render title', () => {
